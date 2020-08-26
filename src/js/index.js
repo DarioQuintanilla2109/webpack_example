3f225654416bfff6f91ef9f1e0ecd5331fcd3db1
Dario Quintanilla <DarioQuintanilla@Darios-iMac.local>
2020-08-26 02:02:03 -0400

    updated randomcard generator + webpack  solution

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,49 @@
 
 window.onload = () => {
-  document.querySelector('.card').classList.add(generateRandomSuit());
-  document.querySelector('.card').innerHTML = (generateRandomNumber());
+  //document.querySelector('.number').innerHTML = (generateRandomNumber());
+  generateRandomCard();
+};
+
+    
+let generateRandomCard = () => {
+  //arrays for suits 
+  var suitsTop = [
+    "hearts",
+    "spades",
+    "diamond",
+    "clubs"
+ ];
+ var suitsBottom = [
+    "hearts",
+    "spades",
+    "diams",
+    "clubs"
+ ];
+ //array for number/value
+ var ranks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+
+ //since we want the same value for both suits, the random index returned gets assigned to both
+ var suitIndex = Math.floor(Math.random() * suitsBottom.length)
+   var suit = [
+      suitsBottom[suitIndex],
+      suitsTop[suitIndex]
+   ];
+   //value of card
+   var rankIndex = Math.floor(Math.random() * ranks.length);
+   var rank = [
+      ranks[rankIndex],
+      
+   ];
+   document.querySelector(".card").innerHTML += 
+   
+   "<span class='top-corner'>&" + suit[1] + 
+   ";</span> <div class='card" + rank[0] + " " + 
+   suit[0] + "'><span class='.top-corner'>" + rank[0] + 
+   "</span><span class='bottom-corner'>&" + suit[1] + 
+   ";</span></div>";
+
+
 };
 
 
-let generateRandomNumber = () => {
-  
-  let numbers = ['A', '2', '3', '4', '5','6','7','8','9','10','J','Q', 'K'];
-  let indexNumbers = Math.floor(Math.random() * numbers.length);
-  return numbers[indexNumbers];
-
-};
-
-
-let generateRandomSuit = () =>{
-
-
-  var suit = ["diamond", "spade", "heart", "club"];
-  let indexSuit = Math.floor(Math.random() * suit.length);
-  return suit[indexSuit];
-};
 
